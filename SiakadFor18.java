c8d4c7ea9bc8f34568683f64107562b7296e6e76
“rendiizaqi” <rendirealme34@gmail.com>
2025-10-23 12:00:11 +0700

    push 2

--- SiakadFor18.java
+++ SiakadFor18.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 
-public class SiakadFor18{
+public class SiakadFor18 {
     public static void main(String[] args) {
 
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidakLulus = 0;
         Scanner sc = new Scanner(System.in);
 
         for (int i = 1; i <= 10; i++) {
@@ -16,9 +17,19 @@ public class SiakadFor18{
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if (nilai >= 60) {
+                lulus++;
+            } else {
+                tidakLulus++;
+            }
         }
 
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiswa lulus: " + lulus);
+        System.out.println("Jumlah mahasiswa tidak lulus: " + tidakLulus);
+
+        sc.close();
     }
 }
